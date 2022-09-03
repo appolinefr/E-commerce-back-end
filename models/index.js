084eaf9850e835fd582150060b0b02007f22fc14
Appoline Cogan <appolinecogan@gmail.com>
2022-09-03 16:47:18 +0700

    created models and associations between then

--- models/index.js
+++ models/index.js
@@ -1,13 +1,25 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
-// Products belongsTo Category
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+});
 
-// Categories have many Products
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+});
 
+Product.belongsToMany(Tag, {
+  foreignKey: "tag_id",
+});
+
+Tag.belongsToMany(Product, {
+  foreignKey: "product_id",
+});
 // Products belongToMany Tags (through ProductTag)
 
 // Tags belongToMany Products (through ProductTag)
